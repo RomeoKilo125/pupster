00853dc695c649ae8a58db98de205a910242cbd1
RomeoKilo125 <romeokilo125@gmail.com>
2019-01-30 18:42:36 -0500

    init commit

---
+++ src/components/pages/Discover.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Discover() {
+
+  return (
+    <div>
+      <p>
+        Discover
+      </p>
+    </div>
+  )
+}
+
+export default Discover
---
+++ src/components/pages/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Search() {
+
+  return (
+    <div>
+      <p>
+        Search
+      </p>
+    </div>
+  )
+}
+
+export default Search
