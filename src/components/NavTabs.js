5c91db1adde5accfbcb3ab8a8ad00bac747ed011
RomeoKilo125 <romeokilo125@gmail.com>
2019-01-30 19:10:45 -0500

    added brand to navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom'
 function NavTabs() {
   return (
     <ul className='nav nav-tabs'>
+      <li className="nav-item">
+        <Link to='/' className='navbar-brand'>Pupster</Link>
+      </li>
       <li className='nav-item'>
         <Link to='/' className={window.location.pathname === '/' ? 'nav-link active' : 'nav-link'}>
           Home
